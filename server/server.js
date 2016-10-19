11ded18d146fc83f765b073b16931d06dd753721
EdG570 <cole570@hotmail.com>
2016-10-18 21:31:02 -0400

    Added POST /todos

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { db } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -6,6 +7,20 @@ const { User } = require('./models/user');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    description: req.body.description
+  });
+
+  todo.save().then((todo) => {
+    res.send(todo);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Express is listening on port 3000');
-}); 
+});
