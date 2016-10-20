f9adf5d4fbd99dacfd7fcf55dd2089b4a73e336d
EdG570 <cole570@hotmail.com>
2016-10-20 16:05:09 -0400

    Successfully adding auth tokens to userinstances and filtering return fields to email and id while excluding password and tokens

--- server/server.js
+++ server/server.js
@@ -105,13 +105,14 @@ app.post('/users', (req, res) => {
     let body = _.pick(req.body, ['email', 'password']);
     const user = new User(body);
 
-    user.save().then((user) => {
-      res.send(user);
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
     }).catch((e) => {
       res.status(400).send(e);
     });
 });
-
 
 app.listen(port, () => {
   console.log(`Express is listening on port: ${port}`);
