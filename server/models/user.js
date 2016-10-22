3c6aae916c13b5e2b01c95094eca3f71cd25e962
EdG570 <cole570@hotmail.com>
2016-10-21 20:26:09 -0400

    POST /users/login auth token generated and returned

--- server/models/user.js
+++ server/models/user.js
@@ -80,9 +80,9 @@ UserSchema.statics.findByCredentials = function(email, password) {
     return new Promise((resolve, reject) => {
       bcrypt.compare(password, user.password, (err, res) => {
         if (res === true) {
-          return resolve(user);
+          resolve(user);
         } else {
-          return reject();
+          reject();
         }
       });
     });
