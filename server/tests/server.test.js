420feea98ae3ce91a3f05d9e132197ebbe61ecb8
EdG570 <cole570@hotmail.com>
2016-10-24 13:15:10 -0400

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -284,7 +284,7 @@ describe('POST /users/login', () => {
       })
       .end((err, res) => {
         if (err) return done(err);
-        
+
         User.findById(users[1]._id).then((user) => {
           expect(user.tokens[0]).toInclude({
             access: 'auth',
@@ -312,9 +312,25 @@ describe('POST /users/login', () => {
 
         User.findById(users[1]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
+          done();
         }).catch((e) => done(e));
+      });
+  });
+});
 
-        done();
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
       });
   });
 });
